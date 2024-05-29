b755bd599bf09f161a27231b040fd8ae684cfd6e
Patrick Roeleveld <roeleveldpatrick@gmail.com>
2024-05-29 11:23:40 +0200

    Opzetje BEM

--- BEM.py
+++ BEM.py
@@ -1,4 +1,11 @@
 def BEM (v0,omega,pitch)
-
+    B = 3;  # number of blades
+    R = 63;  # rotor radius
+    hubrad = 1.5;  # hub radius
+    rou = 1.225;  # density of air
+    EPS = 0.00001;  # iterative precision tolerance
+    for i in range(NBS):
+        #import airfoil & blade data
+        while abs(ax-a)>= EPS:
 
     return Rx, FN,FT
