6996a505567335b43c919826df46e023d01409d4
ogrootes <ogrootes@student.tudelft.nl>
2024-06-06 14:26:40 +0200

    Changed BEM to output Edgewise and Flapwise Forces (per blade) and changed main code to make static plots 3-25 m/s

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from bem import BEM
 import numpy as np
 import matplotlib.pyplot as plt
 
+%test Ole
+
 if __name__ == '__main__':
     v0 = np.array([3, 4, 5, 6, 7, 8, 9, 10, 11, 11.4, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])
     rotational_frequancies = np.array(
